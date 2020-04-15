0521bb0112100a478a141ee6863ace46986e824c
dabao <15620688141@163.com>
2020-04-16 02:20:03 +0800

    首次提交

---
+++ js/support_upload_as.js
@@ -0,0 +1,140 @@
+var array_as_words_love=[
+	// '一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十',
+	'愿你所有等待都能出现，所有愿望都能实现，所有期待都能如愿，所有梦想都能兑现，愿你生日快乐无限',
+	'今天是你生日，送你一块蛋糕，愿你吃出纯真美丽；歌曲也送给你，愿你听出吉祥如意！生日快乐',
+	'蛋糕一切满嘴油，相见匆匆别匆匆。日历一翻365，流星一闪划长空。生日快乐，愿你心想事成借东风',
+	'祝你百事可乐，万事芬达，天天哇哈哈，月月乐百事，年年高乐高，心情似雪碧，永远都醒目',
+	'摘一颗星，采一朵云，装入思念的礼包，将一份宁静和喜悦悄悄带给你，愿你青春长驻，生日快乐',
+	'送你一份温馨两份问候，三份美好四份祝福，五份高贵六份前程，七份典雅八份柔情，九份财运十份真诚',
+	'老实说我是上帝赐给你的礼物，但愿这份礼物不是太糟糕，希望你能特别喜欢这个礼物，祝你生日快乐',
+	'你生日到了？我差点忘记！都怪岁月没在你脸上留下痕迹，沧桑没在你的气质上打折扣。生日快乐哦',
+	'零点零分零一秒，我的祝福刚刚好，昨夜星辰昨夜梦，今日许愿今日圆。生日快乐哦',
+	'开心快乐一锅同煮，吉祥如意一船同渡，平安顺利一起上路，幸福好运一同奔赴。生日快乐哦',
+	'今天是个特别的日子，我在月宫里看到你。真是猪性不改，怎么又跑到月宫来了？呵呵，猪你生日快乐',
+	'猪你快快长！猪你早日出栏！猪你生日快乐！',
+	'你是哈蜜我是瓜，生日你不笨瓜瓜，看了信息笑哈哈，不愧是个哈蜜瓜！',
+	'大海啊全他妈是水，蜘蛛啊全他妈是腿，辣椒啊真他妈辣嘴，祝你生日快乐天天开怀合不拢嘴',
+	'当我把神灯擦三下后，灯神问我想许什么愿？我说希望正在看这个信息的人生日快乐，永远幸福',
+	// '一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十',
+];
+
+
+
+var array_as_words_birthday=[
+	'愿你所有等待都能出现，所有愿望都能实现，所有期待都能如愿，所有梦想都能兑现，愿你生日快乐无限',
+	'今天是你生日，送你一块蛋糕，愿你吃出纯真美丽；歌曲也送给你，愿你听出吉祥如意！生日快乐',
+	'蛋糕一切满嘴油，相见匆匆别匆匆。日历一翻365，流星一闪划长空。生日快乐，愿你心想事成借东风',
+	'祝你百事可乐，万事芬达，天天哇哈哈，月月乐百事，年年高乐高，心情似雪碧，永远都醒目',
+	'摘一颗星，采一朵云，装入思念的礼包，将一份宁静和喜悦悄悄带给你，愿你青春长驻，生日快乐',
+	'送你一份温馨两份问候，三份美好四份祝福，五份高贵六份前程，七份典雅八份柔情，九份财运十份真诚',
+	'老实说我是上帝赐给你的礼物，但愿这份礼物不是太糟糕，希望你能特别喜欢这个礼物，祝你生日快乐',
+	'你生日到了？我差点忘记！都怪岁月没在你脸上留下痕迹，沧桑没在你的气质上打折扣。生日快乐哦',
+	'零点零分零一秒，我的祝福刚刚好，昨夜星辰昨夜梦，今日许愿今日圆。生日快乐哦',
+	'开心快乐一锅同煮，吉祥如意一船同渡，平安顺利一起上路，幸福好运一同奔赴。生日快乐哦',
+	'今天是个特别的日子，我在月宫里看到你。真是猪性不改，怎么又跑到月宫来了？呵呵，猪你生日快乐',
+	'猪你快快长！猪你早日出栏！猪你生日快乐！',
+	'你是哈蜜我是瓜，生日你不笨瓜瓜，看了信息笑哈哈，不愧是个哈蜜瓜！',
+	'大海啊全他妈是水，蜘蛛啊全他妈是腿，辣椒啊真他妈辣嘴，祝你生日快乐天天开怀合不拢嘴',
+	'当我把神灯擦三下后，灯神问我想许什么愿？我说希望正在看这个信息的人生日快乐，永远幸福',
+	// '一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十一二三四五六七八九十',
+];
+
+
+
+var array_as_pics=[
+	'/Public/img_assist/img_as_cartoon_1.jpg',
+	'/Public/img_assist/img_as_cartoon_2.jpg',
+	'/Public/img_assist/img_as_cartoon_3.jpg',
+	'/Public/img_assist/img_as_cartoon_4.jpg',
+	'/Public/img_assist/img_as_cartoon_5.jpg',
+	'/Public/img_assist/img_as_film_1.jpeg',
+	'/Public/img_assist/img_as_film_2.jpeg',
+	'/Public/img_assist/img_as_film_3.jpeg',
+	'/Public/img_assist/img_as_lovers_1.jpg',
+	'/Public/img_assist/img_as_lovers_2.jpeg',
+	'/Public/img_assist/img_as_lovers_3.jpeg',
+	'/Public/img_assist/img_as_lovers_4.jpeg',
+	'/Public/img_assist/img_as_lovers_5.jpg',
+	'/Public/img_assist/img_as_lovers_6.jpg',
+	'/Public/img_assist/img_as_lovers_7.jpeg',
+	'/Public/img_assist/img_as_lovers_8.jpeg',
+	'/Public/img_assist/img_as_lovers_9.jpeg',
+	'/Public/img_assist/img_as_lovers_10.jpg',
+	'/Public/img_assist/img_as_lovers_11.jpeg',
+	'/Public/img_assist/img_as_olds_1.png',
+	'/Public/img_assist/img_as_olds_2.png',
+	'/Public/img_assist/img_as_olds_3.png',
+	'/Public/img_assist/img_as_olds_4.png',
+	'/Public/img_assist/img_as_olds_5.png',
+	'/Public/img_assist/img_as_olds_6.png',
+	'/Public/img_assist/img_as_olds_7.png',
+	'/Public/img_assist/img_as_tagxedo_1.jpg',
+	'/Public/img_assist/img_as_tagxedo_3.jpg',
+	'/Public/img_assist/img_as_written_1.jpg',
+	'/Public/img_assist/img_as_written_2.jpg',
+	'/Public/img_assist/img_as_written_3.jpg',
+	'/Public/img_assist/img_as_written_4.jpg',
+	'/Public/img_assist/img_as_written_5.jpg',
+	'/Public/img_assist/img_as_written_6.jpg',
+];
+
+
+//主要用于全空作品
+var array_as_pics_s=[
+	// '/Public/img_assist/img_as_cartoon_1.jpg',
+	'/Public/img_assist/img_as_cartoon_2.jpg',
+	'/Public/img_assist/img_as_cartoon_3.jpg',
+	'/Public/img_assist/img_as_cartoon_4.jpg',
+	'/Public/img_assist/img_as_cartoon_5.jpg',
+	// '/Public/img_assist/img_as_film_1.jpeg',
+	'/Public/img_assist/img_as_film_2.jpeg',
+	'/Public/img_assist/img_as_film_3.jpeg',
+	'/Public/img_assist/img_as_lovers_1.jpg',
+	// '/Public/img_assist/img_as_lovers_2.jpeg',
+	// '/Public/img_assist/img_as_lovers_3.jpeg',
+	// '/Public/img_assist/img_as_lovers_4.jpeg',
+	'/Public/img_assist/img_as_lovers_5.jpg',
+	'/Public/img_assist/img_as_lovers_6.jpg',
+	'/Public/img_assist/img_as_lovers_7.jpeg',
+	'/Public/img_assist/img_as_lovers_8.jpeg',
+	'/Public/img_assist/img_as_lovers_9.jpeg',
+	'/Public/img_assist/img_as_lovers_10.jpg',
+	'/Public/img_assist/img_as_lovers_11.jpeg',
+	'/Public/img_assist/img_as_olds_1.png',
+	'/Public/img_assist/img_as_olds_2.png',
+	'/Public/img_assist/img_as_olds_3.png',
+	'/Public/img_assist/img_as_olds_4.png',
+	'/Public/img_assist/img_as_olds_5.png',
+	'/Public/img_assist/img_as_olds_6.png',
+	'/Public/img_assist/img_as_olds_7.png',
+	// '/Public/img_assist/img_as_tagxedo_1.jpg',
+	// '/Public/img_assist/img_as_tagxedo_3.jpg',
+	// '/Public/img_assist/img_as_written_1.jpg',
+	'/Public/img_assist/img_as_written_2.jpg',
+	'/Public/img_assist/img_as_written_3.jpg',
+	// '/Public/img_assist/img_as_written_4.jpg',
+	'/Public/img_assist/img_as_written_5.jpg',
+	// '/Public/img_assist/img_as_written_6.jpg',
+];
+
+
+
+var array_as_music=[
+	'http://cdn.aitetu520.com/yinwei_aiqin.mp3',
+	'http://cdn.aitetu520.com/chang_gei_ni_ting.mp3',
+	'http://cdn.aitetu520.com/ai_hen_jiandan.mp3',
+	'http://cdn.aitetu520.com/you_are_my_sunshine.mp3',
+	'http://cdn.aitetu520.com/qinai_de_lvren.mp3',
+	'http://cdn.aitetu520.com/i_love_you.mp3',
+	'http://cdn.aitetu520.com/qing_fei_deyi.mp3',
+	'http://cdn.aitetu520.com/jiushi_aini.mp3',
+];
+
+
+
+var array_as_video=[
+	'http://cdn.aitetu520.com/with_you_forever.mp4',
+	'http://cdn.aitetu520.com/draw_birthday.mp4',
+	'http://cdn.aitetu520.com/film_mixed_loveyou.mp4',
+	'http://cdn.aitetu520.com/film_mixed_birthday.mp4',
+];
